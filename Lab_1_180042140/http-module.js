2c467503a507339f7f31819a741076125294861f
JaberHPranto <jhpranto53@gmail.com>
2021-04-30 05:00:03 +0600

    end point working

--- Lab_1_180042140/http-module.js
+++ Lab_1_180042140/http-module.js
@@ -1,21 +1,41 @@
 const http = require('http');
-const content = require('./loadContent')
-// console.log(content);
+const {aboutHtml,blogHtml,contactHtml,indexHtml,pricingHtml,servicesHtml,workHtml} = require('./loadContent')
+
 
 const server = http.createServer((req, res) => {
     if (req.url == '/') {
-        res.write(`<h1>Hello World<h1>`)
-        res.end()
+        res.write(indexHtml)
     }
-    else if (req.url == '/home')
-    {
-        res.write(content.aboutHtml);
-        res.end();
-        
-    } else {
-        res.write("Page not found :(")
-        res.end()
+    
+    else if (req.url == '/about') {
+        res.write(aboutHtml);; 
     }
+
+    else if (req.url == "/blog") {
+      res.write(blogHtml);
+    }
+    
+    else if (req.url == "/contact") {
+      res.write(contactHtml);
+    }
+    
+    else if (req.url == "pricing") {
+      res.write(pricingHtml);
+    }
+    
+    else if (req.url == "/services") {
+      res.write(servicesHtml);
+    }
+    
+    else if (req.url == "/work") {
+      res.write(workHtml);
+    }
+    
+    else {
+      res.write("Page doesn't exist ");
+    }
+    
+    res.end()
 })
 
 module.exports={server}
