3c36d1e97ae29ce3cbd9494143e6bfa603319bed
JaberHPranto <jhpranto53@gmail.com>
2021-06-25 20:34:44 +0600

    database model create

--- Lab2__Lab3/index.js
+++ Lab2__Lab3/index.js
@@ -13,13 +13,13 @@ app.use(express.urlencoded({ limit: '30mb', extended: true }))
 
 app.use("/user",userRoute)
 
+
 app.get('/',(req,res) => {
     res.send('Welcome')
 })
 
 // connect to mongodb
 const PORT = process.env.PORT || 5000
-
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => { console.log(`Server started on port ${PORT}`) }))
     .catch((err) => console.log("Failed to start", err))
