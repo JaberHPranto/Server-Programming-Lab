accf457b9efc7371c8575995e7f1318d5d615236
JaberHPranto <jhpranto53@gmail.com>
2021-06-25 21:46:58 +0600

    providing signin form

--- Lab2__Lab3/controllers/userController.js
+++ Lab2__Lab3/controllers/userController.js
@@ -1,7 +1,9 @@
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 const User = require('../model/userModel')
-const bcrypt = require('bcrypt');
+
 const getSignInForm = (req,res) => {
-    
+    res.sendFile('signin.html',{root:'./views/users'})
 }
 
 const getSignUpForm = (req,res) => {
@@ -12,25 +14,32 @@ const getSignUpForm = (req,res) => {
 const signIn = async (req, res) => {
     res.send("Sign In")
 }
+
+
 const signUp = async (req, res) => {
     const { name, email, password, confirmPassword } = req.body
     
-    // checking for email
-    const existingUser = await User.findOne({ email })
-    if (existingUser)
-        return res.status(404).json({ message: "User not found" })
-    
-    // checking for password
-    if (password.length < 6)
-        return res.status(404).json({ message: "Password must be at least 6 character" })
-    if (password !== confirmPassword)
-        return res.status(404).json({ message: "Password doesn't match" })
-    
-    // store into database 
-    const hashedPassword = await bcrypt.hash(password, 12)
-    const result = await User.create({ name, email, password: hashedPassword })
+    try {
+        // checking for email
+        const existingUser = await User.findOne({ email })
+        if (existingUser)
+            return res.status(404).json({ message: "User not found" })
+        
+        // checking for password
+        if (password.length < 6)
+            return res.status(404).json({ message: "Password must be at least 6 character" })
+        if (password !== confirmPassword)
+            return res.status(404).json({ message: "Password doesn't match" })
+        
+        // store into database 
+        const hashedPassword = await bcrypt.hash(password, 12)
+        const result = await User.create({ name, email, password: hashedPassword })
 
-    res.send("Sign Up")
+        res.send("Sign Up")
+    } catch (error) {
+        res.status(500).json({message:"Sign up failed",error})
+    }
+    
 }
 
 module.exports = {signIn,signUp,getSignInForm,getSignUpForm}
