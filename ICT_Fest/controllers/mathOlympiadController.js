413f1c5f78cef3bccac2731807ed20b5be55e5b7
JaberHPranto <jhpranto53@gmail.com>
2021-08-03 15:03:56 +0600

    set up mo register form and receive data from it

--- ICT_Fest/controllers/mathOlympiadController.js
+++ ICT_Fest/controllers/mathOlympiadController.js
@@ -2,6 +2,13 @@ const getMO = (req, res) => {
     res.render("math-olympiad/register.ejs")
 }
 const postMO = (req, res) => {
+    const { name, institution, email, contact, category, tshirt } = req.body;
+    console.log(name);
+    console.log(institution);
+    console.log(email);
+    console.log(contact);
+    console.log(category);
+    console.log(tshirt);
     res.render("math-olympiad/register.ejs")
 }
 const getMOList = (req, res) => {
