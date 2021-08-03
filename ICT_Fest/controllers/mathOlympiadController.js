80d5b7dd743e31a4b44b3f48730e31b315b54d18
JaberHPranto <jhpranto53@gmail.com>
2021-08-03 16:08:44 +0600

    participant data can be saved to database from now

--- ICT_Fest/controllers/mathOlympiadController.js
+++ ICT_Fest/controllers/mathOlympiadController.js
@@ -1,14 +1,41 @@
+const MathOlympiad = require("../model/mathOlympiadModel")
 const getMO = (req, res) => {
     res.render("math-olympiad/register.ejs")
 }
-const postMO = (req, res) => {
-    const { name, institution, email, contact, category, tshirt } = req.body;
-    console.log(name);
-    console.log(institution);
-    console.log(email);
-    console.log(contact);
-    console.log(category);
-    console.log(tshirt);
+const postMO = async (req, res) => {
+    try {
+        const { name, institution, email, contact, category, tshirt } = req.body;    
+        let registrationFee = 0;
+        if (category === 'school') registrationFee = 250;
+        else if (category === 'college') registrationFee = 400;
+        else if (category === 'university') registrationFee = 500;
+
+        const total = registrationFee;
+        const paid = 0;
+        const selected = false;
+
+        let error = ""
+
+        const isValidParticipant = await MathOlympiad.find({ name, contact })
+        if (!isValidParticipant) {
+            console.log("Participant with this name and contact number already exist");
+            return res.redirect('/math-olympiad/register')
+        }
+
+        const participant = new MathOlympiad({
+            name,category,contact,email,institution,total,paid,selected,tshirt
+        })
+
+        await participant.save();
+
+
+    } catch (err) {
+        console.log(err);
+        return res.redirect('/math-olympiad/register')
+    }
+
+
+
     res.render("math-olympiad/register.ejs")
 }
 const getMOList = (req, res) => {
